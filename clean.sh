c8c606e8a814906ff52c3ec108f875d6a6bd9c1c
Anton Samarchyan <desecho@gmail.com>
2018-02-04 17:50:35 -0500

    Add comments

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# We want to remove imports before running isort.
 autoflake --remove-all-unused-imports --in-place -r ghcontrib ghcontrib_project
 yapf -ri ghcontrib ghcontrib_project
+# We want to run isort after yapf to make sure isort lint pass.
 isort -rc ghcontrib ghcontrib_project
