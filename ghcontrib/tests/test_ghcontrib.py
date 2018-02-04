2222d92948e162f8ccdd496037934c66687ee796
Anton Samarchyan <desecho@gmail.com>
2018-02-04 04:44:26 -0500

    Add test

--- ghcontrib/tests/test_ghcontrib.py
+++ ghcontrib/tests/test_ghcontrib.py
@@ -1,6 +1,12 @@
+from django.urls import reverse
+
 from .base import BaseTestLoginCase
 
 
 class GithubContribTestCase(BaseTestLoginCase):
     def test_home(self):
-        assert True
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertListEqual(list(response.context_data['usernames']), ['fox', 'ironman', 'neo'])
+
