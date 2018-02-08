c841a82357ee4e419be87e31788afad5fbb8c861
Anton Samarchyan <desecho@gmail.com>
2018-02-08 03:59:21 -0500

    Apply js-beautify

--- ghcontrib/static/js/preferences.js
+++ ghcontrib/static/js/preferences.js
@@ -1,19 +1,25 @@
 app.factory('SavePreferences', ['$resource', function($resource) {
   return $resource(urlSavePreferences, {}, {
-    post: {method: 'POST', headers: headers},
+    post: {
+      method: 'POST',
+      headers: headers
+    },
   });
 }]);
 
 app.controller('PreferencesController', ['$scope', 'SavePreferences',
-function($scope, SavePreferences) {
-  $scope.savePreferences = function(reload) {
-    const preferences = {language: $('input:radio[name=lang]:checked').val()};
-    SavePreferences.post($.param(preferences), function() {
-      if (reload) {
-        location.reload();
-      }
-    }, function() {
-      displayMessage(gettext('Error saving settings'));
-    });
-  };
-}]);
+  function($scope, SavePreferences) {
+    $scope.savePreferences = function(reload) {
+      const preferences = {
+        language: $('input:radio[name=lang]:checked').val()
+      };
+      SavePreferences.post($.param(preferences), function() {
+        if (reload) {
+          location.reload();
+        }
+      }, function() {
+        displayMessage(gettext('Error saving settings'));
+      });
+    };
+  }
+]);
