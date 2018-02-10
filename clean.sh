8b3d11c88900b2e71c2a28c7c3191fe6629555eb
Anton Samarchyan <desecho@gmail.com>
2018-02-10 10:52:14 -0500

    Changes to clean script

--- clean.sh
+++ clean.sh
@@ -2,7 +2,11 @@
 
 # We want to remove imports before running isort.
 autoflake --remove-all-unused-imports --in-place -r ghcontrib ghcontrib_project
-yapf -ri ghcontrib ghcontrib_project
+
+yapf -ri ghcontrib/tests/*.py ghcontrib/views ghcontrib/*.py ghcontrib_project
+# This command takes a long time.
+# yapf -ri ghcontrib/tests/fixtures/*.py
+
 # We want to run isort after yapf to make sure isort lint pass.
 isort -rc ghcontrib ghcontrib_project
 csscomb ghcontrib/static/style.css
