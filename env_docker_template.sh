704cc6cfa1dad7dabdf7c59c3efa34edfe2388a5
Anton Samarchyan <desecho@gmail.com>
2021-04-13 02:04:51 -0400

    Simplify env_docker_template.sh

--- env_docker_template.sh
+++ env_docker_template.sh
@@ -5,19 +5,6 @@ export DEBUG=
 # bool
 export COLLECT_STATIC=
 # bool
-export IS_DEV=
-export SECRET_KEY=key
-export PROJECT_DOMAIN=localhost
-export ADMIN_EMAIL=
-export INTERNAL_IP=
-export RAVEN_DSN=
-export GOOGLE_ANALYTICS_ID=id
 export STATIC_URL=
 
-export DB_USER=root
-export DB_PASSWORD=password
 export DB_HOST=host.docker.internal
-
-export SOCIAL_AUTH_GITHUB_KEY=
-export SOCIAL_AUTH_GITHUB_SECRET=
-export GITHUB_API_KEY=
