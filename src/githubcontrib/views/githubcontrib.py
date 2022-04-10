07acd202ebeb5eb6a2f2574a23778a73cf94ce8c
Anton Samarchyan <desecho@gmail.com>
2022-04-10 16:04:21 -0400

    Optimize query

--- src/githubcontrib/views/githubcontrib.py
+++ src/githubcontrib/views/githubcontrib.py
@@ -31,7 +31,7 @@ class ContribsView(TemplateAnonymousView):
 
     def get_context_data(self, username):  # pylint: disable=no-self-use
         user = get_object_or_404(User, username=username)
-        return {"repos": user.repos.all(), "selected_user": user}
+        return {"repos": user.repos.all().prefetch_related("commits"), "selected_user": user}
 
 
 class MyContribsView(TemplateView):
