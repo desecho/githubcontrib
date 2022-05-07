66e26012bc0cda2abe0e8d3ccfa9eaee85251417
Anton Samarchyan <desecho@gmail.com>
2022-05-07 17:35:09 -0400

    Refactoring

--- src/githubcontrib/admin.py
+++ src/githubcontrib/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
+from django.contrib.admin import site
 
 from .models import Commit, Repo, User
 
-admin.site.register(User)
-admin.site.register(Repo)
-admin.site.register(Commit)
+site.register(User)
+site.register(Repo)
+site.register(Commit)
