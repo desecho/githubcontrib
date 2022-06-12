fdcb23ee6bd503e127ff8723bf8b5fc19b691a00
Anton Samarchyan <desecho@gmail.com>
2022-06-12 00:30:45 -0400

    Cleanup admin

--- src/githubcontrib/admin.py
+++ src/githubcontrib/admin.py
@@ -1,7 +1,14 @@
 from django.contrib.admin import site
+from django.contrib.auth.models import Group
+from social_django.models import Association, Nonce
 
 from .models import Commit, Repo, User
 
 site.register(User)
 site.register(Repo)
 site.register(Commit)
+
+
+site.unregister(Nonce)
+site.unregister(Association)
+site.unregister(Group)
