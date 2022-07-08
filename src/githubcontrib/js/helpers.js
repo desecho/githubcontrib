bbe986b0563b87865a5d14fa9ce4b75263a9f799
Anton Samarchyan <desecho@gmail.com>
2022-07-08 19:46:35 -0400

    Remove unused js code

--- src/githubcontrib/js/helpers.js
+++ src/githubcontrib/js/helpers.js
@@ -3,16 +3,6 @@
 import {loadProgressBar} from 'axios-progress-bar';
 import axios from 'axios';
 
-export function param(params) {
-  const paramsOutput = new URLSearchParams();
-  for (const key in params) {
-    if (Object.prototype.hasOwnProperty.call(params, key)) {
-      paramsOutput.append(key, params[key]);
-    }
-  }
-  return paramsOutput;
-}
-
 export function initAxios(vm) {
   loadProgressBar();
   const headers = {
